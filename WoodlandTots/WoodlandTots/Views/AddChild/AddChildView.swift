e6b52e03a00cdb76df2b344979d8175479bd9ef9
Jayme Rutkoski <jayme-rutkoski@Jayme-Rutkoski-macpro2021.local>
2023-07-18 22:43:33 -0700

    Update UI elements for Add Child

--- WoodlandTots/WoodlandTots/Views/AddChild/AddChildView.swift
+++ WoodlandTots/WoodlandTots/Views/AddChild/AddChildView.swift
@@ -11,16 +11,69 @@ struct AddChildView: View {
 
     @Environment(\.dismiss) private var dismiss
     @ObservedObject var viewModel: ViewModel
+    @State private var name: String = ""
     
     var body: some View {
         return NavigationStack() {
-            VStack() {
+            VStack(alignment: .leading) {
                 
-                Button {
-                    dismiss()
-                } label: {
-                    Text("Test Me")
+                ScrollView() {
+                    HStack {
+                        VStack {
+                            Text("Name:")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            TextField("Enter name", text: $name)
+                                .textFieldStyle(.plain)
+                                .padding(10)
+                                .padding(.leading, 4)
+                                .accentColor(.black)
+                                .background(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1))
+                                .padding(.top, -5)
+                            
+                            Text("Age:")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.top, 10)
+                            TextField("Enter Age", text: $name)
+                                .textFieldStyle(.plain)
+                                .keyboardType(.numberPad)
+                                .padding(10)
+                                .padding(.leading, 4)
+                                .accentColor(.black)
+                                .background(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1))
+                                .padding(.top, -5)
+                            
+                            Text("Note:")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.top, 10)
+                            TextEditor(text: $name)
+                                .textFieldStyle(.plain)
+                                .padding(10)
+                                .padding(.leading, 4)
+                                .accentColor(.black)
+                                .background(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1))
+                                .padding(.top, -5)
+                                .frame(minHeight: 200)
+                            
+                            Button() {
+                                dismiss()
+                            } label: {
+                                Text("Add Child")
+                                            .frame(minWidth: 0, maxWidth: .infinity)
+                                            .font(.system(size: 22))
+                                            .padding()
+                            }
+                            .buttonStyle(.plain)
+                            .padding(10)
+                            .background(.green)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, maxHeight: 60.0)
+                            .cornerRadius(12)
+                            .padding(.top, 5)
+                        }
+                        Spacer()
+                    }
                 }
+                .frame(maxHeight: .infinity)
             }
             .navigationTitle("Add Child")
         }
