44757d322f9c6fb17491062b54106c89adae3856
Jayme Rutkoski <jayme-rutkoski@Jayme-Rutkoski-macpro2021.local>
2023-07-18 20:46:25 -0700

    Add WoodlandTots project

---
+++ WoodlandTots/WoodlandTots/App/WoodlandTotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoodlandTotsApp.swift
+//  WoodlandTots
+//
+//  Created by Jayme Rutkoski on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WoodlandTotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
