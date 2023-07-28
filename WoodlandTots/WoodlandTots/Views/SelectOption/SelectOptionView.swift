45b7e97769ae75b137495ed1c5cca7ee1f5d9ed5
Jayme Rutkoski <jayme-rutkoski@Jayme-Rutkoski-macpro2021.local>
2023-07-27 19:05:51 -0700

    Add a navigationtitle

--- WoodlandTots/WoodlandTots/Views/SelectOption/SelectOptionView.swift
+++ WoodlandTots/WoodlandTots/Views/SelectOption/SelectOptionView.swift
@@ -13,42 +13,46 @@ struct SelectOptionView: View {
     @ObservedObject var viewModel: ViewModel
     
     var body: some View {
-        return ZStack {
-            VStack {
-                List {
-                    ForEach(viewModel.options) { item in
-                        OptionCell(item: item, delegate: self)
-                            .listRowInsets(.init(top: 5, leading: 5, bottom: 5, trailing: 5))
-                            .buttonStyle(PlainButtonStyle())
-                            .listRowBackground(Color.clear)
-                            .listRowSeparator(.hidden)
+        return NavigationView() {
+            ZStack {
+                VStack {
+                    List {
+                        ForEach(viewModel.options) { item in
+                            OptionCell(item: item, delegate: self)
+                                .listRowInsets(.init(top: 5, leading: 5, bottom: 5, trailing: 5))
+                                .buttonStyle(PlainButtonStyle())
+                                .listRowBackground(Color.clear)
+                                .listRowSeparator(.hidden)
+                        }
                     }
+                    .listStyle(PlainListStyle())
+                    .padding(.top, 10)
+                    .padding(.bottom, 65)
                 }
-                .listStyle(PlainListStyle())
-                .padding(.top, 10)
-                .padding(.bottom, 65)
-            }
-            VStack {
-                Spacer()
-                Button() {
-                    self.apply(options: self.viewModel.options.filter( {$0.isSelected} ))
-                } label: {
-                    Text("APPLY")
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .font(.system(size: 22))
-                        .padding()
+                VStack {
+                    Spacer()
+                    Button() {
+                        self.apply(options: self.viewModel.options.filter( {$0.isSelected} ))
+                    } label: {
+                        Text("APPLY")
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .font(.system(size: 22))
+                            .padding()
+                    }
+                    .buttonStyle(.plain)
+                    .padding(10)
+                    .background(.green)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, maxHeight: 60.0)
+                    .cornerRadius(12)
+                    .padding(.top, 5)
                 }
-                .buttonStyle(.plain)
-                .padding(10)
-                .background(.green)
-                .foregroundColor(.white)
-                .frame(maxWidth: .infinity, maxHeight: 60.0)
-                .cornerRadius(12)
-                .padding(.top, 5)
+                .padding(.horizontal, 10)
+                .padding(.bottom, 5)
+                
             }
-            .padding(.horizontal, 10)
-            .padding(.bottom, 5)
-            
+            .navigationTitle("Select an Option")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
     
