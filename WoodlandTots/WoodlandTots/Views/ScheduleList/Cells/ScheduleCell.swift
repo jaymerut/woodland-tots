52dd2113a1077291ec227e2b71fc52dbd57b863a
Jayme Rutkoski <jayme-rutkoski@Jayme-Rutkoski-macpro2021.local>
2023-08-24 20:54:32 -0700

    Update layout to have a two column vertical button column

--- WoodlandTots/WoodlandTots/Views/ScheduleList/Cells/ScheduleCell.swift
+++ WoodlandTots/WoodlandTots/Views/ScheduleList/Cells/ScheduleCell.swift
@@ -11,74 +11,122 @@ struct ScheduleCell: View {
     @ObservedObject var item: ScheduleItem
     var delegate: ScheduleFormProtocol
     
-    @State private var isPresentingForm = false
+    @State private var isPresentingEdit = false
+    @State private var isPresentingMeals = false
     
     var body: some View {
         ZStack {
-            VStack(alignment: .leading) {
-                HStack {
+            NavigationLink(destination: ScheduleFormView(viewModel: .init(model: item, delegate: self.delegate, mode: .edit)), isActive: $isPresentingEdit) {}
+                    .frame(width: 0, height: 0)
+                    .hidden()
+            
+            NavigationLink(destination: ScheduleFormView(viewModel: .init(model: item, delegate: self.delegate, mode: .edit)), isActive: $isPresentingMeals) {}
+                    .frame(width: 0, height: 0)
+                    .hidden()
+            
+            HStack(alignment: .top) {
+                VStack {
                     Text(item.child.name)
                         .font(FontHelper.fontHelveticaBold(22))
                         .multilineTextAlignment(.leading)
                         .frame(maxWidth: .infinity, alignment: .leading)
-                    VStack(alignment: .trailing) {
-                        HStack {
-                            Button() {
-                                isPresentingForm.toggle()
-                            } label: {
-                                Image(systemName: "square.and.pencil")
-                                    .renderingMode(.template)
-                                    .frame(width: 40, height: 40)
-                                    .foregroundColor(.white)
-                                    .background(.green)
-                                    .clipShape(Circle())
+                    
+                    VStack {
+                        ForEach(item.activities) { item in
+                            VStack() {
+                                Text(item.name)
+                                    .font(FontHelper.fontHelveticaBold(18))
+                                    .multilineTextAlignment(.leading)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Text(item.categoryType.rawValue)
+                                    .font(FontHelper.fontHelveticaBold(12))
+                                    .multilineTextAlignment(.trailing)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.top, 1)
+                                Text(item.description)
+                                    .font(FontHelper.fontHelveticaNeueItalic(14.0))
+                                    .multilineTextAlignment(.leading)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.top, 10)
+                                    .visibility(item.description.isEmpty ? .gone : .visible)
                             }
-                            .buttonStyle(.plain)
-                            
-                            
-                            NavigationLink(destination: ScheduleFormView(viewModel: .init(model: item, delegate: self.delegate, mode: .edit)), isActive: $isPresentingForm) {}
-                                    .frame(width: 0, height: 0)
-                                    .hidden()
-            
-                            Image(systemName: "trash.fill")
+                            .padding(.top, 15)
+                        }
+                    }
+                    .padding(.leading, 20)
+                }
+                
+                VStack(alignment: .trailing) {
+                    HStack(alignment: .top) {
+                        Button() {
+                            isPresentingEdit.toggle()
+                        } label: {
+                            Image(systemName: "square.and.pencil")
                                 .renderingMode(.template)
                                 .frame(width: 40, height: 40)
                                 .foregroundColor(.white)
-                                .background(.red)
+                                .background(.gray)
                                 .clipShape(Circle())
-                                .onTapGesture {
-                                    self.delegate.removeSchedule(schedule: item)
-                                }
                         }
+                        .buttonStyle(.plain)
+                        
+        
+                        Image(systemName: "trash.fill")
+                            .renderingMode(.template)
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.white)
+                            .background(.red)
+                            .clipShape(Circle())
+                            .onTapGesture {
+                                self.delegate.removeSchedule(schedule: item)
+                            }
+                    }
+                    
+                    HStack {
+                        Button() {
+                            isPresentingMeals.toggle()
+                        } label: {
+                            Image(systemName: "star.fill")
+                                .renderingMode(.template)
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                                .background(.orange)
+                                .clipShape(Circle())
+                        }
+                        .buttonStyle(.plain)
+                        
+                        Button() {
+                            isPresentingMeals.toggle()
+                        } label: {
+                            Image(systemName: "carrot.fill")
+                                .renderingMode(.template)
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                                .background(.green)
+                                .clipShape(Circle())
+                        }
+                        .buttonStyle(.plain)
+                    }
+                    
+                    HStack {
+                        Button() {
+                            isPresentingMeals.toggle()
+                        } label: {
+                            Image(systemName: "powersleep")
+                                .renderingMode(.template)
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                                .background(.cyan)
+                                .clipShape(Circle())
+                        }
+                        .buttonStyle(.plain)
+                        
                     }
                 }
-                
-                VStack() {
-                    ForEach(item.activities) { item in
-                        VStack() {
-                            Text(item.name)
-                                .font(FontHelper.fontHelveticaBold(18))
-                                .multilineTextAlignment(.leading)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            Text(item.categoryType.rawValue)
-                                .font(FontHelper.fontHelveticaBold(12))
-                                .multilineTextAlignment(.trailing)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(.top, 1)
-                            Text(item.description)
-                                .font(FontHelper.fontHelveticaNeueItalic(14.0))
-                                .multilineTextAlignment(.leading)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(.top, 10)
-                                .visibility(item.description.isEmpty ? .gone : .visible)
-                        }
-                        .padding(.top, 15)
-                    }
-                }
-                .padding(.leading, 20)
             }
             .padding(10)
         }
+        
     }
         
 }
